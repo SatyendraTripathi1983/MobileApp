91094229ea1b6b2d285721d6c66582946f904d2d
Satyendra Mani <dxb.stripathi@gmail.com>
2020-04-24 09:49:32 +0400

    Initial Commit

---
+++ MobileAppAppium/src/main/java/MobileAppAppium/MobileAppAppium/App.java
@@ -0,0 +1,13 @@
+package MobileAppAppium.MobileAppAppium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
